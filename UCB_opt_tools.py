316256407fddc401893505c4f80c286f30e133f5
Jonathan Greenhalgh <jgreenhalgh1126@gmail.com>
2023-03-22 23:11:17 -0600

    Added plots to normal mode UCB-opt ATR demo

--- UCB_opt_tools.py
+++ UCB_opt_tools.py
@@ -14,7 +14,7 @@ def UCB_opt(X_trn, y_trn, X_pred, num_std=1, **gpr_kwargs):
         Outputs:
         X_pred_opt - feature encoding for the sample with the maximum UCB 
         y_pred_opt - the mean predicted value corresponding to the maximum UCB
-        std_pred - confidence interval for the prediction corresponding to the maximum UCB
+        std_preds - confidence interval for the prediction corresponding to the maximum UCB
         UCB_opt_ind - the row index in X_pred corresponding to the maximum UCB 
         '''
 
@@ -22,21 +22,21 @@ def UCB_opt(X_trn, y_trn, X_pred, num_std=1, **gpr_kwargs):
     gpr = GaussianProcessRegressor(**gpr_kwargs)
 
     # Train and predict
-    y_pred, std_pred = gpr.fit(X_trn, y_trn).predict(X_pred, return_std=True)
+    y_preds, std_preds = gpr.fit(X_trn, y_trn).predict(X_pred, return_std=True)
 
     # Calculate UCB (defined as the sum of the mean and the confidence interval)
-    UCB = y_pred + num_std*std_pred
+    UCB = y_preds + num_std*std_preds
 
     # Find the index of the UCB optimum
     UCB_opt_ind = UCB.argmax()
 
     # Find the mean of the prediction corresponding to the UCB optimum
-    y_pred_opt = list(y_pred)[UCB_opt_ind]
+    y_pred_opt = list(y_preds)[UCB_opt_ind]
     X_pred_opt = np.array(X_pred)[UCB_opt_ind]
 
     
-    return X_pred_opt, y_pred_opt, std_pred, UCB_opt_ind
-  
+    return X_pred_opt, y_pred_opt, y_preds, std_preds, UCB_opt_ind
+
   
 def UCB_batch_mode(X_trn, y_trn, X_pred, batchsize, num_std=1, **gpr_kwargs):
 
@@ -52,7 +52,7 @@ def UCB_batch_mode(X_trn, y_trn, X_pred, batchsize, num_std=1, **gpr_kwargs):
         Outputs:
         X_trn_batch - feature encoding for the samples with the maximum UCB 
         y_trn_batch - the mean predicted values corresponding to the maximum UCB
-        std_pred - confidence intervals for the prediction corresponding to the maximum UCB
+        std_preds - confidence intervals for the prediction corresponding to the maximum UCB
         UCB_opt_ind_batch - list of indices that are batch-mode UCB optima for a given round
      
     '''
@@ -61,20 +61,21 @@ def UCB_batch_mode(X_trn, y_trn, X_pred, batchsize, num_std=1, **gpr_kwargs):
     batch = 1 # counter
     X_trn_batch = np.array(X_trn)   # training set features
     y_trn_batch = list(y_trn)       # training set labels 
-    std_pred_batch = [0 for i in y_trn_batch] # set the confidence intervals equal to zero for known measurements 
-
+    # std_pred_batch = [0 for i in y_trn_batch] # set the confidence intervals equal to zero for known measurements 
+    # std_pred_batch = [] 
     X_pred_batch = np.array(X_pred) # prediction set 
 
     UCB_opt_ind_batch = [] # batch-mode UCB optima
     
     while batch <= batchsize:
         # Optimize the UCB 
-        X_top_pred, y_pred, std_pred, UCB_opt_ind = UCB_opt(X_trn_batch, y_trn_batch, X_pred_batch, num_std=num_std, **gpr_kwargs)
+        X_top_pred, y_pred_opt, y_preds, std_preds, UCB_opt_ind = UCB_opt(X_trn_batch, y_trn_batch, X_pred_batch, num_std=num_std, **gpr_kwargs)
 
+        # print(std_preds)
         # Add a psuedo-measurement to the training data by assuming that the predicted value for the UCB optimum will become the new observed value
         # Update the labels with the pseudo-measurement
-        y_trn_batch.append(y_pred)
-        std_pred_batch = std_pred_batch + std_pred 
+        y_trn_batch.append(y_pred_opt)
+        # std_pred_batch = std_pred_batch.append(std_preds)
 
         # Update the the feature list with the pseudo-measurement features
         X_trn_batch = np.vstack([X_trn_batch, X_top_pred])
@@ -87,7 +88,7 @@ def UCB_batch_mode(X_trn, y_trn, X_pred, batchsize, num_std=1, **gpr_kwargs):
         # Update the counter
         batch += 1
 
-    return X_trn_batch, y_trn_batch, std_pred_batch, UCB_opt_ind_batch
+    return X_trn_batch, y_trn_batch, UCB_opt_ind_batch
     
 
 
