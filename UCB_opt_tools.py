00bcf7ccb4b908a631658674ced18306d3952c65
Jonathan Greenhalgh <jgreenhalgh1126@gmail.com>
2023-06-07 21:49:46 -0600

    Wrote custom class for UCB optimization operation

--- UCB_opt_tools.py
+++ UCB_opt_tools.py
@@ -1,117 +1,162 @@
 import numpy as np
 from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.base import BaseEstimator, TransformerMixin 
+from sklearn.utils.validation import check_is_fitted
 
 
-def UCB_opt(X_trn, y_trn, X_pred, num_std=1, **gpr_kwargs):
-    ''' Function for identifying the optimal upper-confidence bound
-        Inputs:
-        X_trn - array of features for training the GP regressor. Row order must correlate with y_trn
-        y_trn - series or vector containing the labels for the GP regressor. Row order must correlate with X_trn
-        X_pred - array of features to be used for prediction
-        num_std - number of standard deviations to use as a multiplier in upper-confidence bound estimation. Default: 1
-        **gpr_kwargs - kwargs that feed into GuassianProcessRegressor (such as kernel, alpha, etc.)
+
+class get_UCB(BaseEstimator, TransformerMixin):
+    def __init__(self, X_trn, y_trn):
+        self.X_trn = X_trn
+        self.y_trn = y_trn
+    
+    def fit(self, **gpr_kwargs):
+        #Initialize GPR
+        gpr = GaussianProcessRegressor(**gpr_kwargs)
+
+        #Fit the GPR
+        self.fit_ = gpr.fit(self.X_trn, self.y_trn)
+
+        return self
+    
+    def transform(self, X, num_stds = 1, return_std=True):
+
+        check_is_fitted('fit_')
+
+        self.y_preds, self.std_preds = self.fit_.predict(X, return_std=return_std)
+
+        self.UCB = self.y_preds + num_stds*self.std_preds
+
+        self.UCB_opt_ind = self.UCB.argmax()
+        self.y_pred_opt = list(self.ypreds)[self.UCB_opt_ind]
+        self.x_pred_opt = np.array(X)[self.UCB_opt_ind]
+
+        return self
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+# def UCB_opt(X_trn, y_trn, X_pred, num_std=1, **gpr_kwargs):
+#     ''' Function for identifying the optimal upper-confidence bound
+#         Inputs:
+#         X_trn - array of features for training the GP regressor. Row order must correlate with y_trn
+#         y_trn - series or vector containing the labels for the GP regressor. Row order must correlate with X_trn
+#         X_pred - array of features to be used for prediction
+#         num_std - number of standard deviations to use as a multiplier in upper-confidence bound estimation. Default: 1
+#         **gpr_kwargs - kwargs that feed into GuassianProcessRegressor (such as kernel, alpha, etc.)
         
-        Outputs:
-        X_pred_opt - feature encoding for the sample with the maximum UCB 
-        y_pred_opt - the mean predicted value corresponding to the maximum UCB
-        std_preds - confidence interval for the prediction corresponding to the maximum UCB
-        UCB_opt_ind - the row index in X_pred corresponding to the maximum UCB 
-        '''
+#         Outputs:
+#         X_pred_opt - feature encoding for the sample with the maximum UCB 
+#         y_pred_opt - the mean predicted value corresponding to the maximum UCB
+#         std_preds - confidence interval for the prediction corresponding to the maximum UCB
+#         UCB_opt_ind - the row index in X_pred corresponding to the maximum UCB 
+#         '''
 
-    # Intialize the Gaussian Process Regressor
-    gpr = GaussianProcessRegressor(**gpr_kwargs)
+#     # Intialize the Gaussian Process Regressor
+#     gpr = GaussianProcessRegressor(**gpr_kwargs)
 
-    # Train and predict
-    y_preds, std_preds = gpr.fit(X_trn, y_trn).predict(X_pred, return_std=True)
+#     # Train and predict
+#     y_preds, std_preds = gpr.fit(X_trn, y_trn).predict(X_pred, return_std=True)
 
-    # Calculate UCB (defined as the sum of the mean and the confidence interval)
-    UCB = y_preds + num_std*std_preds
+#     # Calculate UCB (defined as the sum of the mean and the confidence interval)
+#     UCB = y_preds + num_std*std_preds
 
-    # Find the index of the UCB optimum
-    UCB_opt_ind = UCB.argmax()
+#     # Find the index of the UCB optimum
+#     UCB_opt_ind = UCB.argmax()
 
-    # Find the mean of the prediction corresponding to the UCB optimum
-    y_pred_opt = list(y_preds)[UCB_opt_ind]
-    X_pred_opt = np.array(X_pred)[UCB_opt_ind]
+#     # Find the mean of the prediction corresponding to the UCB optimum
+#     y_pred_opt = list(y_preds)[UCB_opt_ind]
+#     X_pred_opt = np.array(X_pred)[UCB_opt_ind]
 
     
-    return X_pred_opt, y_pred_opt, y_preds, std_preds, UCB_opt_ind
+#     return X_pred_opt, y_pred_opt, y_preds, std_preds, UCB_opt_ind
 
   
-def UCB_batch_mode(X_trn, y_trn, X_pred, batchsize, num_std=1, **gpr_kwargs):
+# def UCB_batch_mode(X_trn, y_trn, X_pred, batchsize, num_std=1, **gpr_kwargs):
 
-    '''  Function for identifying the optimal upper-confidence bound
-        Inputs:
-        X_trn - array of features for training the GP regressor. Row order must correlate with y_trn
-        y_trn - series or vector containing the labels for the GP regressor. Row order must correlate with X_trn
-        X_pred - array of features to be used for prediction
-        batchsize - number of samples to use in the batch
-        num_std - number of standard deviations to use as a multiplier in upper-confidence bound estimation. Default: 1
-        **gpr_kwargs - kwargs that feed into GuassianProcessRegressor (such as kernel, alpha, etc.)
+#     '''  Function for identifying the optimal upper-confidence bound
+#         Inputs:
+#         X_trn - array of features for training the GP regressor. Row order must correlate with y_trn
+#         y_trn - series or vector containing the labels for the GP regressor. Row order must correlate with X_trn
+#         X_pred - array of features to be used for prediction
+#         batchsize - number of samples to use in the batch
+#         num_std - number of standard deviations to use as a multiplier in upper-confidence bound estimation. Default: 1
+#         **gpr_kwargs - kwargs that feed into GuassianProcessRegressor (such as kernel, alpha, etc.)
         
-        Outputs:
-        X_top_batch - feature encoding for the samples with the maximum UCB 
-        y_top_batch - the mean predicted values corresponding to the maximum UCB
-        X_preds - encodings for all samples in prediction space
-        y_preds - mean predictions 
-        std_preds - confidence intervals for the prediction corresponding to the maximum UCB
-        UCB_opt_ind_batch - list of indices that are batch-mode UCB optima for a given round
+#         Outputs:
+#         X_top_batch - feature encoding for the samples with the maximum UCB 
+#         y_top_batch - the mean predicted values corresponding to the maximum UCB
+#         X_preds - encodings for all samples in prediction space
+#         y_preds - mean predictions 
+#         std_preds - confidence intervals for the prediction corresponding to the maximum UCB
+#         UCB_opt_ind_batch - list of indices that are batch-mode UCB optima for a given round
      
-    '''
+#     '''
        
-    # Initialize variables 
-    batch = 1 # counter
-    X_trn_batch = np.array(X_trn)   # training set features
-    y_trn_batch = list( np.log(y_trn))       # training set labels 
-    X_pred_batch = np.array(X_pred) # prediction set 
+#     # Initialize variables 
+#     batch = 1 # counter
+#     X_trn_batch = np.array(X_trn)   # training set features
+#     y_trn_batch = list( np.log(y_trn))       # training set labels 
+#     X_pred_batch = np.array(X_pred) # prediction set 
 
-    UCB_opt_ind_batch = [] # batch-mode UCB optima
-    X_top_batch = [] 
-    y_top_batch = [] 
+#     UCB_opt_ind_batch = [] # batch-mode UCB optima
+#     X_top_batch = [] 
+#     y_top_batch = [] 
 
-    while batch <= batchsize:
-        # Optimize the UCB 
-        X_top_pred, y_pred_opt, y_preds, std_preds, UCB_opt_ind = UCB_opt(X_trn_batch, y_trn_batch, X_pred_batch, num_std=num_std, **gpr_kwargs)
+#     while batch <= batchsize:
+#         # Optimize the UCB 
+#         X_top_pred, y_pred_opt, y_preds, std_preds, UCB_opt_ind = UCB_opt(X_trn_batch, y_trn_batch, X_pred_batch, num_std=num_std, **gpr_kwargs)
 
-        # Add the top prediction to the batch
-        X_top_batch.append(X_top_pred)
-        y_top_batch.append(y_pred_opt)
+#         # Add the top prediction to the batch
+#         X_top_batch.append(X_top_pred)
+#         y_top_batch.append(y_pred_opt)
 
 
-        # Add a psuedo-measurement to the training data by assuming that the predicted value for the UCB optimum will become the new observed value
-        y_trn_batch.append(y_pred_opt)
+#         # Add a psuedo-measurement to the training data by assuming that the predicted value for the UCB optimum will become the new observed value
+#         y_trn_batch.append(y_pred_opt)
 
-        # Update the the feature list with the pseudo-measurement features
-        X_trn_batch = np.vstack([X_trn_batch, X_top_pred])
+#         # Update the the feature list with the pseudo-measurement features
+#         X_trn_batch = np.vstack([X_trn_batch, X_top_pred])
 
-        # Update the batch-mode list
-        UCB_opt_ind_batch.append(UCB_opt_ind)
+#         # Update the batch-mode list
+#         UCB_opt_ind_batch.append(UCB_opt_ind)
     
-        # Update the counter
-        batch += 1
+#         # Update the counter
+#         batch += 1
 
 
-    if len(UCB_opt_ind_batch) != len(set(UCB_opt_ind_batch)):
-        print('Warning: local optimum in batch')
+#     if len(UCB_opt_ind_batch) != len(set(UCB_opt_ind_batch)):
+#         print('Warning: local optimum in batch')
 
-        # Combine X_top_batch and y_top_batch into a dataframe 
-        df_Xy = pd.DataFrame(X_top_batch)
-        df_Xy['y'] = y_top_batch
+#         # Combine X_top_batch and y_top_batch into a dataframe 
+#         df_Xy = pd.DataFrame(X_top_batch)
+#         df_Xy['y'] = y_top_batch
 
-        # Remove duplicates 
-        df_Xy = df_Xy.drop_duplicates(subset = df_Xy.columns != 'y')
-        print(f'Removed {batchsize-len(df_Xy)} duplicates')
+#         # Remove duplicates 
+#         df_Xy = df_Xy.drop_duplicates(subset = df_Xy.columns != 'y')
+#         print(f'Removed {batchsize-len(df_Xy)} duplicates')
 
-        # Reassign X_top_batch and y_top_batch 
-        X_top_batch = df_Xy.loc[:, df_Xy.columns != 'y'].to_numpy()
-        y_top_batch = list(df_Xy['y'])
+#         # Reassign X_top_batch and y_top_batch 
+#         X_top_batch = df_Xy.loc[:, df_Xy.columns != 'y'].to_numpy()
+#         y_top_batch = list(df_Xy['y'])
 
-        UCB_opt_ind_batch = list(set(UCB_opt_ind_batch))
+#         UCB_opt_ind_batch = list(set(UCB_opt_ind_batch))
 
-    X_preds = X_pred_batch
+#     X_preds = X_pred_batch
 
-    # return X_trn_batch, y_trn_batch, X_preds, y_preds, std_preds, UCB_opt_ind_batch
-    return X_top_batch, y_top_batch, X_preds, y_preds, std_preds, UCB_opt_ind_batch
+#     # return X_trn_batch, y_trn_batch, X_preds, y_preds, std_preds, UCB_opt_ind_batch
+#     return X_top_batch, y_top_batch, X_preds, y_preds, std_preds, UCB_opt_ind_batch
 
     
 
